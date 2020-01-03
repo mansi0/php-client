73da5e7b5ea92527751e9990b804579164e08ae7
mansi0 <mansimandhare.mm@gmail.com>
2020-01-03 15:12:00 +0530

    6-customerLogin created

---
+++ public/rough.php
@@ -0,0 +1,123 @@
+<body>
+    <div class="main">
+    <section class="header">
+        <div class="content-box">
+         <div class="menu">
+               <!-- <img src="./projectphoto/logo.png">-->
+            <ul>
+                <li> <a href="login.html"><i class="fas fa-sign-in-alt"></i>Login</a></li>
+                <li> <a href="reg1.html"><i class="fas fa-user-plus"></i>Register</a></li>
+            </ul>
+         </div>
+        <div class="banner-text">
+            <h2 >Taste On Way...</h2>
+            <!--<a href="#">About us</a>-->
+        </div>     
+        <!--<slider>
+              <slide><p>Slide 1</p></slide>
+              <slide><p>Slide 2</p></slide>
+              <slide><p>Slide 3</p></slide>
+              <slide><p>Slide 4</p></slide>
+        </slider>-->
+        </div>
+
+    </section>
+
+    <!--service-->
+    <section class="service">
+        <div class="content-box">
+            <div class="container1">
+                <div class="box">
+                 <div class="content">
+                 <h1>01</h1>
+                 <h2>Join Our Business</h2>
+                 <a href="#">Read More</a>
+                 </div>
+                </div>
+
+
+             <div class="container1">
+                 <div class="box">
+                    <div class="content">
+                        <h1>02</h1>
+                        <h2>Work With Us</h2>
+                        <a href="#">Read More</a>
+                    </div>
+                 </div>
+         </div>
+
+    </section>
+    </div>
+
+    <section class="features">
+        <div class="content-box">
+            <div class="container2">
+                <div class="card">
+                    <div class="imgBx" data-text="Feature1">
+                        <img src="./projectphoto/f2.png">
+                    </div>
+                <div class="content">
+                <div>
+                    <h3>Feature1</h3>
+                    <p>Self-Pickup</p>
+                    <a href="#">Read More</a>
+                </div>
+            </div>
+        </div>
+
+            <div class="card">
+                <div class="imgBx" data-text="Feature2">
+                <img src="./projectphoto/f2.png">
+            </div>
+            <div class="content">
+            <div>
+                <h3>Feature2</h3>
+                <p>Express-Delivery</p>
+                <a href="#">Read More</a>
+            </div>
+         </div>
+    </div>
+
+            <div class="card">
+            <div class="imgBx" data-text="Feature3">
+            <img src="./projectphoto/f2.png">
+            </div>
+            <div class="content">
+            <div>
+                <h3>Feature3</h3>
+                <p>Items are categorized.</p>
+                <a href="#">Read More</a>
+            </div>
+        </div>
+    </div>
+
+            <div class="card">
+            <div class="imgBx" data-text="Feature4">
+             <img src="./projectphoto/f2.png">
+            </div>
+            <div class="content">
+            <div>
+                <h3>Feature4</h3>
+                <p>features will be added later</p>
+                <a href="#">Read More</a>
+            </div>
+        </div>
+    </div>
+
+</div>
+</section>
+</div>
+
+
+    <script>
+        $(".main").tiltedpage_scroll({
+            sectionContainer:"> section",
+            angle:50,
+            opacity:true,
+            scale:true,
+            outAnimation:true
+        });
+    </script>
+</body>
+</html>
+    
---
+++ public/runningpage.php
@@ -0,0 +1,46 @@
+<?php
+  $ini_array = parse_ini_file("../config/config.ini", true);
+  $url = $ini_array['url'];
+?>
+
+<html>
+    <head>
+        <title>Running vehicle animation </title>
+        <link rel="stylesheet" href="./runningpage.css">
+    </head>
+    <body>
+            <div class="hero">
+                   
+            <div class="highway"></div>
+            <div class="city"></div>
+            <div class="vehicle">
+                    <img src="./projectphoto/1576083508486.png">
+            </div>
+           
+            <div class="wheel">
+               <img src="./projectphoto/Car_Animation_Img/wheel.png" class="back-wheel">
+              <img src="./projectphoto/Car_Animation_Img/wheel.png" class="frontwheel">
+            </div>
+            <div class="title">
+                    <img src="./projectphoto/1576163963556.png">
+            </div>
+
+           <!-- <header>
+              <div class="main">
+                    <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Services</a></li>
+                            <li><a href="#">Gallery</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </header>-->
+   
+            <!--<button onclick="window.location.href = 'home.html';">Order Now>></button>-->
+                   <!-- <a href="/home/aishwariya/fooddelivery/demo/src/main/resources/reg2.html"> -->
+                    <a href="reg1.html">Order Now>></a>
+            
+        </div>
+    </body>
+</html>
