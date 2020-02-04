b05df7c934088bbae2c9a0d6dbda750109f920aa
mansi0 <mansimandhare.mm@gmail.com>
2020-02-04 14:14:18 +0530

    hoteldisplay to food display

---
+++ public/js/foodDisplay.js
@@ -0,0 +1,34 @@
+async function food(hotelId) {
+    try {
+
+        alert("in foodDisplay");
+
+        //var hotelId=document.getElementById('hotelid').value;
+        alert(hotelId);
+        // Creating a cookie after the document is ready 
+        
+            createCookie("hotelId", hotelId, "10");
+    
+
+        // Function to create the cookie 
+        function createCookie(name, value, days) {
+            var expires;
+
+            if (days) {
+                var date = new Date();
+                date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+                expires = "; expires=" + date.toGMTString();
+            }
+            else {
+                expires = "";
+            }
+
+            document.cookie = escape(name) + "=" +
+                escape(value) + expires + "; path=/";
+        }
+        window.location.replace("../public/foodDisplay1.php");
+
+    } catch (error) {
+        console.log(error);
+    }
+}
